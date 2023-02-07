90296bb1bbe21c9cdcc4429b7be32340c81a8f0d
leese00 <122573963+leese00@users.noreply.github.com>
2023-02-07 16:30:37 -0500

    Completes exercise

--- Player/Bullet.gd
+++ Player/Bullet.gd
@@ -4,6 +4,8 @@ var velocity = Vector2.ZERO
 var speed = 500.0
 var damage = 1
 
+onready var Explosion = load("res://Explosion.tscn")
+var Effects = null 
 
 func _ready():
 	velocity = Vector2(0,-speed).rotated(rotation)
@@ -16,6 +18,13 @@ func _physics_process(_delta):
 
 
 func _on_Area2D_body_entered(body):
+	if body.has_method("damage"):
+		body.damage(damage)
+	Effects = get_node_or_null("/root/Game/Effects")
+	if Effects != null:
+		var explosion = Explosion.instance()
+		Effects.add_child(explosion)
+		explosion.global_position = global_position
 	queue_free()
 
 
